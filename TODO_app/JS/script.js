07fa543231d4c62c53b41333db2bbe0705170864
Julia Kopichenko <j.titorenko@gmail.com>
2021-04-21 20:58:53 +0300

    feat:  add search

--- TODO_app/JS/script.js
+++ TODO_app/JS/script.js
@@ -1,7 +1,8 @@
-const addTask = document.querySelector('.add-input');
-const addButton = document.querySelector('.add-button');
-const addWrapper = document.querySelector('.add-wrapper');
+const addInput = document.querySelector('.add-input'); // поле ввода новой задачи
+const addButton = document.querySelector('.add-button'); // кнопка ADD
+const addWrapper = document.querySelector('.add-wrapper'); // секция с полем ввода и кнокпой ввода
 let todo = document.querySelector('.todo-list'); //список дел (ul)
+let searchInput = document.querySelector('.search-input'); // поле поиска
 
 // создадим пустой массив, в который будем записывать кждое новое дело (newTodo)
 let todoList = [];
@@ -20,25 +21,25 @@ if (localStorage.getItem('todo') != undefined) {
 // синтаксис JSON.stringify(value[, replacer[, space]])
 // value - Значение, преобразуемое в строку JSON формате.
 function updateLocalStorage() {
-  localStorage.setItem('todo', JSON.stringify(todoList))
+	localStorage.setItem('todo', JSON.stringify(todoList));
 }
 
 //!  функция добавления новой задачи (будет вызвана при клике на кнопку ADD, или при нажатии Enter)
 function addNewTask() {
-	if (!addTask.value) return; // проверка, если пустой ввод
-		
+	if (!addInput.value) return; // проверка, если пустой ввод
 	// каждое новое дело будем записывать в объект, который бкдем добавлять в массив
 	let newTodo = {
-		todo: addTask.value,
+		todo: addInput.value,
 		done: false, // свойство выполнена задача или нет
 		important: false,
 	};
-	// обращается к созданному массиву и  внего с пом.метода push добавляем новое дело
+	// обращается к созданному массиву и  внего с пом.метода push добавляем новое дело (объект newTodo)
 	todoList.push(newTodo);
+
 	//  вызываем функции displayTasks каждый раз, когда нажимаем кнопку
 	displayTasks();
 	
-	addTask.value = ''; // после выполнения обновляется инпут
+	addInput.value = ''; // после выполнения обновляется инпут
 	updateLocalStorage();
 	addAllListeners();
 }
@@ -47,77 +48,97 @@ function addNewTask() {
 addButton.addEventListener('click', function () {
 	addNewTask()
 });
-//!  пишем обработчик события при нажатии на кнопку Enter
-addTask.addEventListener('keydown', function (event) {
-	if (event.code === 'Enter') addNewTask();
+//!  пишем обработчик события при нажатии на кнопку Enter в поле ввода задачи
+addInput.addEventListener('keydown', function (event) {
+	if (event.keyCode == 13 && !event.shiftKey) { // исключаем срабатывае=ние Enter при нажатом shift
+		
+		addNewTask();
+		// event.preventDefault() сообщает, что если событие не обрабатывается явно,
+		// его действие по умолчанию не должно выполняться так, как обычно
+		event.preventDefault();
+	}
 });
 
-//! 11 Функция, которая будет выводить список дел на страницу
+//! Функция, которая будет выводить список дел на страницу
 function displayTasks() {
   //  перебираем массив todoList и каждый объект выводим на страницу в виде тега li
-  //Метод forEach() выполняет указанную функцию один раз для каждого элемента в массиве.
+  // Метод forEach() выполняет указанную функцию один раз для каждого элемента в массиве.
+	// объявляем пустую строку 
   let displayTask = '';
 	todoList.forEach(function (item, index) {
 		displayTask += `
     <li tabindex="0" class="todo-item ${item.done ? 'todo-task--done' : ''} ${item.important ? 'todo-task--important' : ''}" id="${index}">
       <p class="todo-task">${item.todo}</p>  
-      <div class="important-button important-button--not-active" tabindex="0"></div>
-      <div class="bin-button" tabindex="0"></div>
+      <button class="important-button important-button--not-active" tabindex="0"></button>
+      <button class="bin-button" tabindex="0"></button>
     </li>
     `;
 		//  меняем содержимое HTML страницы
 		todo.innerHTML = displayTask;
-		updateLocalStorage();
 	});
 }
 
 function addAllListeners() {
   // найдем все пункты списка дел (все li)
   let todoItems = document.querySelectorAll('.todo-item');
-
-  todoItems.forEach(item => {
+  
+	todoItems.forEach(item => {
     // при клике на поле задачи делаем ее выполненной
-    makeTaskDone(item);
+		makeTaskDoneByClick(item);
+		makeTaskDoneByEnter(item);
 
     // при клике на кнопку important-button
     makeImportantTask(item);
-
     // при клике на корзину
     deleteTask(item);
-
-    updateLocalStorage();
   });
 }
 addAllListeners();
 
-// функция для отметки задачи выполненной
-function makeTaskDone(item) {
-  item.addEventListener('click', function(event) {
-    // Свойство target интерфейса Event является ссылкой на объект, который был инициатором события. 
-    let eventTarget = event.target;
-    // Свойство Node.parentElement только для чтения, возвращает родителя узла DOM Element
+//! функции для отметки задачи выполненной
+// по клику
+function makeTaskDoneByClick(item) {
+	item.addEventListener('click', function (event) {
+		// Свойство target интерфейса Event является ссылкой на объект, который был инициатором события. 
+		let eventTarget = event.target;
+		// Свойство Node.parentElement только для чтения, возвращает родителя узла DOM Element
 		let eventTargetParent = eventTarget.parentElement;
 		
-    // условие, что при клике на кнопки important-button и корзина функция не срабатывала (не зачеркивала)
-    if (eventTarget.classList.contains('important-button')) return;
+		// условие, что при клике на кнопки important-button и корзина функция не срабатывала (не зачеркивала)
+		if (eventTarget.classList.contains('important-button')) return;
     
-    if (eventTarget.classList.contains('bin-button')) return;
+		if (eventTarget.classList.contains('bin-button')) return;
     
-    if (eventTarget.classList.contains ('todo-item')) {
-      // toggle ( String [, Boolean]) Если класс у элемента отсутствует - добавляет, иначе - убирает. 
-      eventTarget.classList.toggle('todo-task--done');
-      let itemId = eventTarget.getAttribute('id');
-      todoList[itemId].done = !todoList[itemId].done;
-      // проверка, если клик по тексту (дочерний элемент li), то родителю класс все равно присваиваем
-    } else if (eventTargetParent.classList.contains ('todo-item')) {
-      eventTargetParent.classList.toggle('todo-task--done');
-      let itemId = eventTargetParent.getAttribute('id');
-      todoList[itemId].done = !todoList[itemId].done;
-    }
-    updateLocalStorage();
-  });
+		if (eventTarget.classList.contains('todo-item')) {
+			// elem.classList.toggle("class") – добавить класс, если его нет, иначе удалить. 
+			eventTarget.classList.toggle('todo-task--done');
+			let itemId = eventTarget.getAttribute('id');
+			todoList[itemId].done = !todoList[itemId].done;
+			// проверка, если клик по тексту (дочерний элемент li), то родителю класс все равно присваиваем
+		} else if (eventTargetParent.classList.contains('todo-item')) {
+			eventTargetParent.classList.toggle('todo-task--done');
+			let itemId = eventTargetParent.getAttribute('id');
+			todoList[itemId].done = !todoList[itemId].done;
+		}
+		updateLocalStorage();
+	});
 }
-// функция преобразования задачи в важную или нет
+// по enter	 (удобно при использовании только клавы)
+function makeTaskDoneByEnter(item) {
+	item.addEventListener('keydown', function (event) {
+		if (event.keyCode == 13) {
+			let eventTarget = event.target;
+			if (eventTarget.classList.contains('todo-item')) {
+				eventTarget.classList.toggle('todo-task--done');
+				let itemId = eventTarget.getAttribute('id');
+				todoList[itemId].done = !todoList[itemId].done;
+			}
+		}					
+		updateLocalStorage();
+	});
+}
+
+//! функция преобразования задачи в важную или нет
 function makeImportantTask(item) {
   item.querySelector('.important-button').addEventListener('click', function(event) {
     let parentElement = event.target.parentElement;
@@ -126,18 +147,21 @@ function makeImportantTask(item) {
     updateLocalStorage();
   });
 }
-// функция для удаления задачи 
-//! пофиксить
+//! функция для удаления задачи 
 function deleteTask(item) {
   item.querySelector('.bin-button').addEventListener('click', function(event) {
-    let parentElement = event.target.parentElement;
-    todoList.splice(parentElement.getAttribute('id'), 1);
-    parentElement.remove();
+		let parentElement = event.target.parentElement;
+		let itemIndex = parentElement.getAttribute('id');
+		parentElement.remove();
+		for (let i = +itemIndex + 1; i < todoList.length; i++) {
+			document.getElementById(i).setAttribute('id', i - 1);
+		}
+    todoList.splice(itemIndex, 1);
     updateLocalStorage();
   }); 
 }
 
-// переключение табов стилизация
+//! переключение табов стилизация
 document.querySelector('.tab-list').addEventListener('click',function(event) {
   let element = event.target; 
 	let parentElement = element.parentElement;
@@ -160,7 +184,7 @@ document.querySelector('.tab-list').addEventListener('click',function(event) {
   }
 });
 
-// по нажатию на закладку All отображаем все задачи
+//! по нажатию на закладку All отображаем все задачи
 document.getElementById('all').addEventListener('click', function() {
   addWrapper.style.display = 'block'; // секция ввод задачи отображается
   todo.classList.add('todo-list--all');
@@ -168,7 +192,7 @@ document.getElementById('all').addEventListener('click', function() {
   todo.classList.remove('todo-list--active');
 });
 
-// 
+// ! по нажатию на закладку active отображаем все важные задачи
 document.getElementById('active').addEventListener('click', function() {
   addWrapper.style.display = 'block'; // секция ввод задачи отображается
   todo.classList.add('todo-list--active');
@@ -176,10 +200,34 @@ document.getElementById('active').addEventListener('click', function() {
   todo.classList.remove('todo-list--done');
 });
 
-//
+//! по нажатию на закладку done отображаем все выполненные задачи
 document.getElementById('done').addEventListener('click', function() {
   addWrapper.style.display = 'none'; // секция ввода задачи не отображается
   todo.classList.add('todo-list--done');
   todo.classList.remove('todo-list--active');
   todo.classList.remove('todo-list--all');
+});
+console.log(todoList);
+
+//! функция поиска
+function searchTasks() {
+	let filter = searchInput.value.toUpperCase();
+// найдем все пункты списка дел (все li)
+let todoItems = document.querySelectorAll('.todo-item');
+	// перебираем все элементы списка и скрываем те, которые не соотв.поисковому запросу
+	for (let i = 0; i < todoItems.length; i++) {
+		let p = todoItems[i].getElementsByTagName("p")[0];
+		if (p.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      todoItems[i].style.display = "";
+    } else {
+      todoItems[i].style.display = "none";
+    }
+	}
+}
+
+searchInput.addEventListener('keydown', function (event) {
+	if (event.keyCode == 13 && !event.shiftKey) { // исключаем срабатывае=ние Enter при нажатом shift
+		searchTasks();
+		event.preventDefault();
+	}
 });
